61f3ca6b7cc0e5116b928793704a4e1b9d5e295a
vantai <vantai08122003@gmail.com>
2023-10-08 23:56:01 +0700

    Add Page Home

---
+++ SportStoreManager/GUI/FormHome.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace GUI
+{
+    partial class FormHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHome));
+            this.picHome = new System.Windows.Forms.PictureBox();
+            this.timerImg = new System.Windows.Forms.Timer(this.components);
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picHome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picHome
+            // 
+            this.picHome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picHome.Location = new System.Drawing.Point(0, 0);
+            this.picHome.Name = "picHome";
+            this.picHome.Size = new System.Drawing.Size(1287, 601);
+            this.picHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picHome.TabIndex = 0;
+            this.picHome.TabStop = false;
+            // 
+            // timerImg
+            // 
+            this.timerImg.Enabled = true;
+            this.timerImg.Interval = 1000;
+            this.timerImg.Tick += new System.EventHandler(this.timerImg_Tick);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "Img1.jpg");
+            this.imageList.Images.SetKeyName(1, "Img2.jpg");
+            this.imageList.Images.SetKeyName(2, "Img3.jpg");
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 601);
+            this.Controls.Add(this.picHome);
+            this.Name = "FormHome";
+            this.Text = "FormHome";
+            ((System.ComponentModel.ISupportInitialize)(this.picHome)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picHome;
+        private System.Windows.Forms.Timer timerImg;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
---
+++ SportStoreManager/GUI/FormHome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FormHome : Form
+    {
+        int i = 0;
+        public FormHome()
+        {
+            InitializeComponent();
+        }
+
+        private void timerImg_Tick(object sender, EventArgs e)
+        {
+            picHome.Image = imageList.Images[i];
+            if(i==imageList.Images.Count-1) {
+                i = 0;
+            }
+            else
+            {
+                i++;
+            }
+
+        }
+    }
+}
